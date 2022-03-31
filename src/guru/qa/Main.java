872bfa9c2e12ebce125a76a2582ee1d77017da39
Mikhail <bulanjova@mail.ru>
2022-03-31 16:33:17 +0300

    Added new Line about slow branch

--- src/guru/qa/Main.java
+++ src/guru/qa/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello,GURU.QA!");
         System.out.println("Another Line");
+        System.out.println("slow branch");
     }
 }
